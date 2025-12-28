0cc562a409e03a72a4bdea3fe89ee1bda03961e3
rmathis <ryanmathis@ccimail.com>
2025-12-28 13:54:06 -0500

    Fix

--- RecipeApps/RecipeWinForms/Program.cs
+++ RecipeApps/RecipeWinForms/Program.cs
@@ -13,7 +13,7 @@ namespace RecipeWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            SQLUtility.ConnectionString = "Server=tcp:dev-cpurm.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info=False;User ID=dev_login; Password = Yuza591886;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SQLUtility.ConnectionString = "Server=tcp:dev-cpurm.database.windows.net,1433;Initial Catalog=HeartyHearthDB;Persist Security Info=False;User ID=User; Password = Password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             Application.Run(new frmSearch());
         }
     }
